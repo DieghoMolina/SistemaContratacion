fa0b1c5c5d54155fc94527bc740a67fed8a33aa8
Marvin Antillon <Marvin Antillon@DESKTOP-26M0VE8>
2017-10-18 15:01:57 -0600

    prueba marvin solo hice un comentario

--- Proyecto/Sistema vacante/src/java/controlador/ProcesarCurriculum.java
+++ Proyecto/Sistema vacante/src/java/controlador/ProcesarCurriculum.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package controlador;
-
+//marvinnclkxckmsx
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
